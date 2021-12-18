85407de265a4290d502eca90848300a4dcafc171
minhld <ledinhminh3883@gmail.com>
2021-12-18 17:43:36 -0600

    defined some attributes and updated styles

--- pages/Home.js
+++ pages/Home.js
@@ -8,16 +8,18 @@ import {
 import { WebView } from 'react-native-webview';
 import Autocomplete from 'react-native-autocomplete-input';
 import { Tab, TabView } from "react-native-elements";
-
+import * as Constants from './utils/Constants';
 import * as db from './utils/DbUtils';
 
 class Home extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       wordHtml: '',
       suggestList: [],
       index: 0,
+      selectedWord: '',
     };
   }
 
@@ -54,6 +56,7 @@ class Home extends React.Component {
       wordHtml: db.getHtml(word),
       suggestList: [],
       index: 0,
+      selectedWord: word.word,
     });
   };
 
@@ -63,6 +66,12 @@ class Home extends React.Component {
     });
   };
 
+  changeContainerStyle = (index) => {
+    return {
+      backgroundColor: (this.state.index === index ? Constants.colors.lightBlue : Constants.colors.background),
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -70,7 +79,8 @@ class Home extends React.Component {
           data={this.state.suggestList}
           style={styles.input}
           onChangeText={(keywords) => this.searchWord(keywords)}
-          placeholder="Enter keywords"
+          defaultValue={this.state.selectedWord}
+          placeholder='Enter keywords'
           flatListProps={{
             keyExtractor: (_, idx) => idx,
             renderItem: ({ item }) => 
@@ -82,12 +92,18 @@ class Home extends React.Component {
         <Tab value={this.state.index} 
           onChange={this.changeTabIndex} 
           indicatorStyle={styles.indicatorTabStyle}>
-          <Tab.Item title="Basic" titleStyle={styles.basicTabTitle}/>
-          <Tab.Item title="English" titleStyle={styles.basicTabTitle}/>
-          <Tab.Item title="Wiki" titleStyle={styles.basicTabTitle}/>
+          <Tab.Item title="Basic" titleStyle={styles.basicTabTitle} 
+            containerStyle={this.changeContainerStyle(0)}
+          />
+          <Tab.Item title="English" titleStyle={styles.basicTabTitle}
+            containerStyle={this.changeContainerStyle(1)}
+          />
+          <Tab.Item title="Wiki" titleStyle={styles.basicTabTitle}
+            containerStyle={this.changeContainerStyle(2)}
+          />
         </Tab>
 
-        <TabView value={this.state.index} >
+        <TabView value={this.state.index}>
           <TabView.Item style={styles.basicTab}>
             <WebView 
               style={styles.textView} 
@@ -117,29 +133,31 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     padding: 10,
-    borderColor: '#1f3c69',
-    backgroundColor: '#fff',
+    borderColor: Constants.colors.defaultBlue,
+    backgroundColor: Constants.colors.defaultWhite,
   },
   itemText: {
     height: 20,
     margin: 8,
   },
   indicatorTabStyle: {
-    backgroundColor: '#1f3c69',
+    backgroundColor: Constants.colors.defaultBlue,
     height: 3,
   },
   basicTabTitle: {
     fontSize: 15,
-    color: '#1f3c69',
+    color: Constants.colors.defaultBlue,
   },
   basicTab: {
     width: '100%',
   },
   englishTab: {
     width: '100%',
+    backgroundColor: Constants.colors.defaultWhite,
   },
   wikiTab: {
     width: '100%',
+    backgroundColor: Constants.colors.defaultWhite,
   },
   textView: {
     fontSize: 30,
--- pages/utils/Constants.js
+++ pages/utils/Constants.js
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import { DefaultTheme } from '@react-navigation/native';
 
 export const DB_URL = 'https://dl.dropboxusercontent.com/s/3vfyfpg8f37zdz7/ev.db';
 export const DB_FILENAME = 'ev.db';
@@ -6,6 +7,13 @@ export const DB_SQLITE_FOLDER = 'SQLite/';
 
 export const getDbPath = () => {
   return FileSystem.documentDirectory + DB_SQLITE_FOLDER;
+};
+
+export const colors = {
+  ...DefaultTheme.colors,
+  defaultBlue: '#1f3c69',
+  lightBlue: '#d5e3f7',
+  defaultWhite: '#fff',
 };
 
 export const getDbFilePath = () => {
