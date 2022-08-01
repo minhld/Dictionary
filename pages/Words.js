5de8b8ed40aa2e1aa0a55be2bcd8f5a958d5cabc
minhld <ledinhminh3883@gmail.com>
2022-08-01 16:34:25 -0500

    removed redundant files

--- pages/Words.js
+++ pages/Words.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
 const Words = ({ navigation }) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("use-effect-1-count: " + count);
+    return () => {
+      // setCount(0);
+    };
+  });
+
+  useEffect(() => {
+    console.log("use-effect-2-count: " + count);
+  });
+
   return (
     <View>
-      <Text>Words!</Text>
+      <Text>${count}</Text>
+      <Button onPress={() => setCount(count + 1)} title="Increase Count"/>
     </View>
   );
 };
